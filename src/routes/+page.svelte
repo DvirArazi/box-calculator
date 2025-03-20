b93d7960b94e289cad073d5b93466579de56007f
dvirarazi0@gmail.com <dvirarazi0@gmail.com>
2025-03-20 09:37:15 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<h1>Despiece Muebles</h1>
+
+<div class="column">
+	<div>
+		Thickness <input type="number" />
+	</div>
+
+	<div>
+		Material HT
+		<select>
+			<option>Sandwich</option>
+			<option>Birch</option>
+			<option>MDF</option>
+		</select>
+	</div>
+
+	<div>
+		Mueble
+	</div>
+
+</div>
+
+<style>
+	.column {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+	}
+</style>
